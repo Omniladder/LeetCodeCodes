8d8adb25875db102b37ee6451dfae7b7cb0a6f61
Dustin T. O'Brien <131387361+Omniladder@users.noreply.github.com>
2024-01-17 20:09:10 -0500

    Time: 47 ms (38.53%), Space: 17.3 MB (49.50%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        occurenceMap = {}
+        for i in arr:
+            if i not in occurenceMap:
+                occurenceMap[i] = 1
+            else:
+                occurenceMap[i] += 1
+        
+        timesMap = set()
+        checkSet = set()
+        
+        for i in arr:
+            if i not in checkSet:
+                if occurenceMap[i] in timesMap:
+                    print(i)
+                    return False
+                else:
+                    timesMap.add(occurenceMap[i])
+                    checkSet.add(i)
+        return True
